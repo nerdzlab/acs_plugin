d0fdcef7c1d5fc971738e82663011c092a9a4e64
dmytrogulko <dmytrogulko@nerdzlab.com>
2025-05-02 12:36:01 +0200

    Fixed layout errors and added activity aware listener for plugin

--- android/src/main/kotlin/com/acs_plugin/AcsPlugin.kt
+++ android/src/main/kotlin/com/acs_plugin/AcsPlugin.kt
@@ -1,29 +1,32 @@
 package com.acs_plugin
 
+import android.app.Activity
 import android.content.Context
 import com.acs_plugin.calling.CallComposite
 import com.acs_plugin.calling.CallCompositeBuilder
-import com.acs_plugin.calling.models.CallCompositeGroupCallLocator
 import com.acs_plugin.calling.models.CallCompositeJoinLocator
+import com.acs_plugin.calling.models.CallCompositeRoomLocator
 import com.azure.android.communication.common.CommunicationTokenCredential
 import com.azure.android.communication.common.CommunicationTokenRefreshOptions
 import com.azure.android.communication.common.CommunicationUserIdentifier
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import java.util.UUID
 
 /** AcsPlugin */
-class AcsPlugin: FlutterPlugin, MethodCallHandler {
+class AcsPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
 
+  private var activity: Activity? = null
   private lateinit var context: Context
 
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
@@ -33,17 +36,23 @@ class AcsPlugin: FlutterPlugin, MethodCallHandler {
     channel.setMethodCallHandler(this)
   }
 
+  // ActivityAware callbacks:
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) { activity = binding.activity }
+  override fun onDetachedFromActivityForConfigChanges() { activity = null }
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) { activity = binding.activity }
+  override fun onDetachedFromActivity() { activity = null }
+
   override fun onMethodCall(call: MethodCall, result: Result) {
     when (call.method) {
       "getPlatformVersion" -> result.success("Android ${android.os.Build.VERSION.RELEASE}")
-      "initializeRoom" -> {
+      "initializeRoomCall" -> {
         val args = call.arguments as? Map<*, *>
         val token = args?.get("token") as? String
         val roomId = args?.get("roomId") as? String
         val userId = args?.get("userId") as? String
 
         if (token != null && roomId != null && userId != null) {
-          initializeRoomCall(token, roomId, userId)
+          initializeRoomCall(token, roomId, userId, result)
         } else {
           result.error(
             "INVALID_ARGUMENTS",
@@ -61,11 +70,16 @@ class AcsPlugin: FlutterPlugin, MethodCallHandler {
     channel.setMethodCallHandler(null)
   }
 
-  private fun initializeRoomCall(token: String, roomId: String, userId: String) {
+  private fun initializeRoomCall(token: String, roomId: String, userId: String, result: MethodChannel.Result) {
+    if (activity == null) {
+      result.error("NO_ACTIVITY", "Plugin not attached to an Activity", null)
+      return
+    }
+
     val communicationTokenRefreshOptions = CommunicationTokenRefreshOptions({ token }, true)
     val communicationTokenCredential = CommunicationTokenCredential(communicationTokenRefreshOptions)
 
-    val locator: CallCompositeJoinLocator = CallCompositeGroupCallLocator(UUID.fromString(roomId))
+    val locator: CallCompositeJoinLocator = CallCompositeRoomLocator(roomId)
     val callComposite: CallComposite = CallCompositeBuilder()
       .applicationContext(this.context)
       .credential(communicationTokenCredential)
@@ -73,6 +87,7 @@ class AcsPlugin: FlutterPlugin, MethodCallHandler {
       .userId(CommunicationUserIdentifier(userId))
       .build()
 
-    callComposite.launch(this.context, locator)
+    callComposite.launch(this.activity, locator)
+    result.success(null)
   }
 }
