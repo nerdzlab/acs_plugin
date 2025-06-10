cccfd1ecf4b4ab5bf11ec465b05f99ee3bb60eb9
andriiservatiak <andriiservatiak@nerdzlab.com>
2025-06-10 18:57:37 +0300

    refactor: rename `userData` to `userDataClass` in UserDataHandler for clarity, remove unused `CallAgent` in CallHandler, and add vector drawable for call icon integration

--- example/android/app/src/main/kotlin/com/example/acs_plugin_example/MyAppConfiguration.kt
+++ example/android/app/src/main/kotlin/com/example/acs_plugin_example/MyAppConfiguration.kt
@@ -15,13 +15,7 @@ import kotlinx.serialization.json.Json
 
 class MyAppConfiguration : Application(), Configuration.Provider {
 
-    var exceptionHandler: Consumer<Throwable?> = object : Consumer<Throwable?> {
-
-        override fun accept(t: Throwable?) {
-
-        }
-
-    }
+    private var exceptionHandler: Consumer<Throwable?> = Consumer<Throwable?> { }
 
     private val sharedPreferences: SharedPreferences by lazy {
         applicationContext.getSharedPreferences(Constants.Prefs.PREFS_NAME, Context.MODE_PRIVATE)
@@ -47,7 +41,7 @@ class MyAppConfiguration : Application(), Configuration.Provider {
 
     override fun getWorkManagerConfiguration(): Configuration = Configuration.Builder().setWorkerFactory(
         RegistrationRenewalWorkerFactory(
-            CommunicationTokenCredential("eyJhbGciOiJSUzI1NiIsImtpZCI6IkRCQTFENTczNEY1MzM4QkRENjRGNjA4NjE2QTQ5NzFCOTEwNjU5QjAiLCJ4NXQiOiIyNkhWYzA5VE9MM1dUMkNHRnFTWEc1RUdXYkEiLCJ0eXAiOiJKV1QifQ.eyJza3lwZWlkIjoiYWNzOjZkMTQxM2NmLTJkMjQtNDE5MS1hNTcwLTExZGE5MTZlODQyNV8wMDAwMDAyNy1iNjUwLWUxODQtNWI0Mi1hZDNhMGQwMDRkNjEiLCJzY3AiOjE3OTIsImNzaSI6IjE3NDk1NDU5NjAiLCJleHAiOjE3NDk2MzIzNjAsInJnbiI6ImRlIiwiYWNzU2NvcGUiOiJjaGF0LHZvaXAiLCJyZXNvdXJjZUlkIjoiNmQxNDEzY2YtMmQyNC00MTkxLWE1NzAtMTFkYTkxNmU4NDI1IiwicmVzb3VyY2VMb2NhdGlvbiI6Imdlcm1hbnkiLCJpYXQiOjE3NDk1NDU5NjB9.i1XZbKIVqYBxhQAj3mxB76pBdTM5PB-dtlc_4b76p-qWNWeYyl7-ZYlnW6alNY8IO9nsWUv60TITSszIB_jYQs2akUyCHhT9BqMBtgxQpJncEv45TXfaH1dR0NVtbW_c17GlMKXTpCPzScJPAVxKTgRY1qHTM5UnuoVf2efulQO7x9jOqzYjxJTmeVH9rFFL77kIsxiTkw4I9zirzMa5L8aDA39X9RXPgcB7pZWCUwmmdUFh0Cgx959jSkgilVEcoqlqpBvoO3EXrJqF0ReJvgoxzlHMtx1eZwFX68-ZHQG8BGeWnMZwi6gHQro5h-mq1Xj2UcnVhfpr97CNdG3eBQ"),
+            CommunicationTokenCredential(userData?.token ?: ""),
             exceptionHandler
         )
     ).build()
